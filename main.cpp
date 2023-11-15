dab14a89398065f2007625448a414813705e8a19
Laurent Giustignano <laurent.giustignano@free.fr>
2023-11-15 23:46:11 +0100

    Exo3.2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <cassert>
 
+unsigned int miroir(unsigned int nombre) {
+    const unsigned int dimensionNonOptimisee = 100;
+    unsigned char conversionNombre[dimensionNonOptimisee];
+    int indice, puissance, resultat;
+
+    for (indice = 0; nombre > 0; nombre /= 10, ++indice) {
+        conversionNombre[indice] = nombre % 10;
+    }
+
+    for (--indice, resultat = 0, puissance = 1; indice >= 0; puissance *= 10, --indice)
+        resultat += conversionNombre[indice] * puissance;
+
+    return resultat;
+}
+
 bool isPalindrome(unsigned int nombre) {
     const unsigned int dimensionNonOptimisee = 100;
     unsigned char conversionNombre[dimensionNonOptimisee];
@@ -11,8 +26,7 @@ bool isPalindrome(unsigned int nombre) {
         conversionNombre[fin] = nombre % 10;
     }
 
-    for (debut = 0, --fin; debut < fin && conversionNombre[debut] == conversionNombre[fin]; ++debut, --fin)
-        ;
+    for (debut = 0, --fin; debut < fin && conversionNombre[debut] == conversionNombre[fin]; ++debut, --fin);
 
     return debut >= fin;
 }
@@ -27,6 +41,15 @@ int main() {
     assert(isPalindrome(123) == false);
 
     std::cout << "Fin Palindrome" << std::endl;
+
+    std::cout << "Debut Miroir" << std::endl;
+
+    assert(miroir(1234) == 4321);
+    assert(miroir(11) == 11);
+    assert(miroir(1) == 1);
+    assert(miroir(123) != 123);
+
+    std::cout << "Fin Miroir" << std::endl;
 
     return 0;
 }
